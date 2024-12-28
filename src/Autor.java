09790cf2ed994234a7737ad47db7b1a046de4215
Thauã <tagashi.dev@gmail.com>
2024-12-28 15:50:45 -0300

    Classe de autor criada

---
+++ src/Autor.java
@@ -0,0 +1,22 @@
+import java.time.LocalDate;
+
+public class Autor {
+    private final Long id;
+    private final String nome;
+    private final LocalDate dataNascimento;
+
+    public Autor(Long id, String nome, LocalDate dataNascimento)
+    {
+        this.id = id;
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+    }
+
+    public void exibirAutor()
+    {
+        System.out.println("Id: " + id);
+        System.out.println("Nome: " + nome);
+        System.out.println("Data de nascimento: " + dataNascimento);
+    }
+    
+}
